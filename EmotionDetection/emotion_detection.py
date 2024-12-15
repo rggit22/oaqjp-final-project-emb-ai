0fca1b73561a1d5b71787634bc618fb3c5a3c367
rggit22 <rishabh.gupta9827@gmail.com>
2024-12-15 09:39:30 -0500

    Inital Commit

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,22 @@
+import requests, json
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = { "raw_document": { "text": text_to_analyze } }
+
+    response = requests.post(url, json = input_json, headers = header)
+
+    response_dict = response.json()
+
+    emotion = response_dict['emotionPredictions'][0]['emotion']
+
+    max_value = 0
+    for key in emotion.keys():
+        if max_value < emotion[key]:
+            max_value = emotion[key]
+            max_key = key
+
+    emotion['dominant_emotion'] = max_key
+
+    return emotion
