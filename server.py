1e541ac6b5e6cf529888b5df5eab7605b4e9597d
rggit22 <rishabh.gupta9827@gmail.com>
2024-12-18 07:44:45 -0500

    Statis code analysis

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+"""
+This is a flask app
+"""
 from flask import Flask, render_template, request
 
 from EmotionDetection.emotion_detection import emotion_detector
@@ -6,28 +9,32 @@ app = Flask(__name__)
 
 @app.route("/emotionDetector")
 def emo_det():
+    """
+    This function generates output for entered text
+    """
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
 
     # Pass the text to the emotion_detector function and store the response
     response = emotion_detector(text_to_analyze)
-    
+
     # Check if dominant_emotion is None and return accordingly
     if response['dominant_emotion'] is None:
         return "Invalid text! Please try again!"  # Return 400 with custom error message
-    else:
-        # Generate a descriptive response from the dictionary
-        formatted_response = (
-            f"For the given statement, the system response is "
-            f"'anger': {response['anger']}, 'disgust': {response['disgust']}, "
-            f"'fear': {response['fear']}, 'joy': {response['joy']} and "
-            f"'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}."
-        )
-        
-        return formatted_response, 200
+    # Generate a descriptive response from the dictionary
+    formatted_response = (
+        f"For the given statement, the system response is "
+        f"'anger': {response['anger']}, 'disgust': {response['disgust']}, "
+        f"'fear': {response['fear']}, 'joy': {response['joy']} and "
+        f"'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}."
+    )
+    return formatted_response, 200
 
 @app.route("/")
 def render_index_page():
+    """
+    This function is use to render HTML template
+    """
     return render_template('index.html')
 
 if __name__ == "__main__":
